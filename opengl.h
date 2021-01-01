9e9f6638770ad552f1dd4c8cf6fb2cb596c3c856
Initial commit <truslnilen@gmail.com>
2021-01-01 04:51:18 +0100

    01.01.2021

--- opengl.h
+++ opengl.h
@@ -1,13 +1,6 @@
 #ifndef _OPENGL_
 #define _OPENGL_
 
-void APIENTRY Hooked_glBegin(GLenum mode);
-void APIENTRY Hooked_glColor4f(GLfloat red, GLfloat green, GLfloat blue, GLfloat alpha);
-BOOL APIENTRY Hooked_wglSwapBuffers(HDC hdc);
-void APIENTRY Hooked_glViewport(GLint x, GLint y, GLsizei width, GLsizei height);
-void APIENTRY Hooked_glClear(GLbitfield mask);
-void APIENTRY Hooked_glReadPixels(GLint x, GLint y, GLsizei width, GLsizei height, GLenum format, GLenum type, GLvoid* pixels);
-
 void HookOpenGL();
 
 #endif
